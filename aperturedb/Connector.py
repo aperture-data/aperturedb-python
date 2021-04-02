7971765b51ee8ecbdf3ab6ac49d620ebdcaf28cc
Luis Remis <luis@aperturedata.io>
2021-04-02 21:40:18 +0000

    Add methods to Connector

--- aperturedb/Connector.py
+++ aperturedb/Connector.py
@@ -1,8 +1,11 @@
 import os
+import sys
+import time
 
 import vdms
 
-from . import Images
+BACKOFF_TIME = 0.2 # seconds
+BACKOFF_MULTIPLIER = 1.2
 
 class Connector(object):
 
@@ -14,19 +17,52 @@ class Connector(object):
         self.connector = vdms.vdms()
         self.connector.connect(ip, port)
 
+        self.last_query_time = 0
+
     def __del__(self):
 
         self.connector.disconnect()
 
-    def query(self, query, blob_array = []):
+    def query(self, q, blobs=[], n_retries=0):
 
-        self.response, self.blobs = self.connector.query(query, blob_array)
+        if n_retries == 0:
+            start = time.time()
+            self.response, self.blobs = self.connector.query(q, blobs)
+            self.last_query_time = time.time() - start
+            return self.response, self.blobs
+
+        status  = -1
+        retries = 0
+        wait_time = BACKOFF_TIME
+        while (status < 0) :
+
+            if retries > 0:
+                time.sleep(wait_time)
+                wait_time *= BACKOFF_MULTIPLIER
+                sys.stderr.write("Retry: " + str(retries) + "\n")
+                sys.stderr.write(db.get_last_response_str())
+
+            if retries > n_retries:
+                print("Warning: Query failed after", n_retries, "retries.")
+                print(self.connector.get_last_response_str())
+                break
+
+            start = time.time()
+            self.response, self.blobs = self.connector.query(q, blobs)
+            self.last_query_time = time.time() - start
+            status = self.check_status(self.response)
+
+            retries += 1
 
         return self.response, self.blobs
 
     def get_last_response_str(self):
 
         return self.connector.get_last_response_str()
+
+    def get_last_query_time(self):
+
+        return self.last_query_time
 
     def get_response(self):
 
@@ -36,9 +72,24 @@ class Connector(object):
 
         return self.blobs
 
-    def get_image_handler(self):
+    # Return a flag on whether the last query succeeded:
+    # i.e., all commands return status >= 0
+    def last_query_ok(self):
 
-        db = vdms.vdms()
-        db.connect(self.ip, self.port)
+        return self.check_status(self.response) >= 0
 
-        return Images.Images(db)
+    def check_status(self, json_res):
+
+        status = 0
+        if (isinstance(self.response, dict)):
+            if ("status" not in self.response):
+                status = self.check_status(json_res[list(json_res.keys())[0]])
+            else:
+                status = json_res["status"]
+        elif (isinstance(json_res, (tuple, list))):
+            if ("status" not in json_res[0]):
+                status = self.check_status(json_res[0])
+            else:
+                status = json_res[0]["status"]
+
+        return status
