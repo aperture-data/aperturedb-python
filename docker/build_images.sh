c8ecf62edd8ea5a636703b1118c245169b35dbba
Luis Remis <luis@remis.io>
2021-04-20 18:17:00 -0700

    Update build_docker script

--- docker/build_images.sh
+++ docker/build_images.sh
@@ -5,3 +5,5 @@ VERSION=$(awk '$1=="__version__" && $2=="=" {print $3}' ../aperturedb/__init__.p
 echo "Building Jupyter Notebook docker image..."
 ( cd notebook && \
   docker build -t aperturedata/aperturedb-notebook:v$VERSION . )
+
+docker push aperturedata/aperturedb-notebook:v$VERSION
