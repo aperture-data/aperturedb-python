a57f7c412bcaa67afb28641c63b6d72a320eed0f
Luis Remis <luis@remis.io>
2021-11-06 11:30:14 -0700

    Update protobuf message

--- aperturedb/queryMessage_pb2.py
+++ aperturedb/queryMessage_pb2.py
@@ -20,7 +20,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='VDMS.protobufs',
   syntax='proto3',
   serialized_options=None,
-  serialized_pb=_b('\n\x12queryMessage.proto\x12\x0eVDMS.protobufs\"+\n\x0cqueryMessage\x12\x0c\n\x04json\x18\x01 \x01(\t\x12\r\n\x05\x62lobs\x18\x02 \x03(\x0c\x62\x06proto3')
+  serialized_pb=_b('\n\x12queryMessage.proto\x12\x0eVDMS.protobufs\":\n\x0cqueryMessage\x12\x0c\n\x04json\x18\x01 \x01(\t\x12\r\n\x05\x62lobs\x18\x02 \x03(\x0c\x12\r\n\x05token\x18\x03 \x01(\tb\x06proto3')
 )
 
 
@@ -47,6 +47,13 @@ _QUERYMESSAGE = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='token', full_name='VDMS.protobufs.queryMessage.token', index=2,
+      number=3, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
   ],
   extensions=[
   ],
@@ -60,7 +67,7 @@ _QUERYMESSAGE = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=38,
-  serialized_end=81,
+  serialized_end=96,
 )
 
 DESCRIPTOR.message_types_by_name['queryMessage'] = _QUERYMESSAGE
