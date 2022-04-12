c743c42ab42fcc4600279e743157452255c44fc8
luisremis <luis@aperturedata.io>
2022-04-11 17:22:57 -0700

    Version 0.1.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="aperturedb",
-    version="0.1.4",
+    version="0.1.5",
     description="ApertureDB Client Module",
     install_requires=['vdms', 'scikit-image', 'image',
                       'opencv-python', 'numpy', 'matplotlib', 'pandas'],
