5499baa7f8fa103a0fea2a271745061f8ca92b71
luisremis <luis@aperturedata.io>
2022-06-09 17:47:29 -0700

    Upgrade protobuf generated file using protoc 3.20.0 (#91)
    * Upgrade protobuf generated file using protoc 3.20.0
    
    * Bump up version to 0.2.0 (protobuf change)
    
    * Update requirements.txt

--- test/run_test.sh
+++ test/run_test.sh
@@ -3,7 +3,7 @@
 set -u
 set -e
 
-(cd .. && pip3 install . && pip3 install -r requirements.txt)
+(cd .. && pip3 install -r requirements.txt && pip3 install .)
 
 sudo rm -rf aperturedb/db
 rm -rf output
