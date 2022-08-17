c885f12149f582e1ce75e56d73bdd6105b2db7ef
Gautam <gautam@aperturedata.io>
2022-08-16 20:41:57 -0400

    Changes to push to ECR.
    Also pushes the test coverage report.

---
+++ test/coverage.sh
@@ -0,0 +1,26 @@
+set -e
+cd ..
+source $(dirname "$0")/ci.sh
+cd -
+
+
+build_coverage_image(){
+    echo "Preping coverage image"
+    coverage html
+    COV_IMAGE=$DOCKER_REPOSITORY/aperturedb-python-coverage${IMAGE_EXTENSION_WITH_VERSION}
+    echo "Building image $COV_IMAGE"
+    docker build -t $COV_IMAGE -f coverage/Dockerfile .
+    if [ -z ${NO_PUSH+x} ]
+    then
+        docker push $COV_IMAGE
+    fi 
+}
+
+build_coverage_image
+
+ECR_REPO_NAME=aperturedb-python-coverage
+COV_IMAGE=$DOCKER_REPOSITORY/$ECR_REPO_NAME${IMAGE_EXTENSION_WITH_VERSION}
+ECR_NAME=$ECR_REPO_NAME:v$BUILD_VERSION
+    
+
+push_aws_ecr $COV_IMAGE $ECR_NAME $ECR_REPO_NAME
