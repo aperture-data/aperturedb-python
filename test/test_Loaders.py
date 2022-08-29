107c5dcfc506d7e0f6e7d6e9fa497f8260904a06
Gautam <gautam@aperturedata.io>
2022-08-29 19:05:55 -0400

    Removing unused imports. Also using fixtures for repetetive code.

--- test/test_Loaders.py
+++ test/test_Loaders.py
@@ -1,5 +1,3 @@
-import time
-
 import numpy as np
 
 from test_Base import TestBase
--- test/test_Utils.py
+++ test/test_Utils.py
@@ -1,17 +1,5 @@
-import time
+class TestUtils():
 
-from test_Base import TestBase
-from aperturedb import Utils
-
-import logging
-logger = logging.getLogger(__name__)
-
-
-class TestUtils(TestBase):
-
-    def test_remove_all_objects(self):
-
-        utils = Utils.Utils(self.create_connection())
-
-        self.assertTrue(utils.remove_all_objects(),
-                        "Failed to remove all objects")
+    def test_remove_all_objects(self, utils):
+        assert utils.remove_all_objects() == True,\
+            "Failed to remove all objects"
