d56f0f4bdab1a05208806d88ec8507614d97b50c
Gautam <gautam@aperturedata.io>
2022-11-29 19:07:18 +0000

    Fixing tests.

--- aperturedb/Blobs.py
+++ aperturedb/Blobs.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from typing import Dict
 
 from aperturedb.Connector import Connector
 from aperturedb.Entities import Entities
@@ -9,5 +10,14 @@ class Blobs(Entities):
     db_object = "_Blob"
 
     @classmethod
-    def retrieve(cls, db: Connector, spec: Query) -> Blobs:
-        return super().retrieve(db, spec)[-1]
+    def retrieve(cls,
+                 db: Connector,
+                 spec: Query,
+                 with_adjacent: Dict[str, Query] = None) -> Blobs:
+        spec.with_class = cls.db_object
+
+        results = Entities.retrieve(
+            db=db, spec=spec, with_adjacent=with_adjacent)
+
+        blobs = results[-1]
+        return blobs
