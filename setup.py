14a3779b5db162e012e3c5fc89611a762a986a8a
Gautam <gautam@aperturedata.io>
2023-01-17 12:22:23 -0500

    Removing the pins that have been addressed with newer versions.
    Also cleans up the dockerfiles to build python loader of opencv.

--- setup.py
+++ setup.py
@@ -8,15 +8,11 @@ setuptools.setup(
     version="0.3.6",
     description="ApertureDB Client Module",
     install_requires=['scikit-image', 'image', 'requests', 'boto3',
-                      'opencv-python', 'numpy', 'matplotlib', 'pandas', 'kaggle', 'google-cloud-storage',
+                      'numpy', 'matplotlib', 'pandas', 'kaggle', 'google-cloud-storage',
                       'dask[complete]', 'ipywidgets',
                       # Pinning this to be able to install tensorflow.
-                      'protobuf<3.20.0',
-                      # Pinning the following 2 packages to avoid a conflict with the latest version of dask.
-                      # TODO: Remove these pins once the conflict is resolved.
-                      # TODO: Explore other package mgmt optins (pyproject.toml), or pip-tools to
-                      # avoid such conflicts in the future.
-                      'tornado==6.1', 'jupyter-client==7.3.4'],
+                      'protobuf<3.20.0'
+                      ],
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/aperture-data/aperturedb-python",
