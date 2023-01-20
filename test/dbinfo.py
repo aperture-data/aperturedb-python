61eae4b325345cbd790a72d6f5f9eedab1087dbc
Gautam Saluja <52312085+gsaluja9@users.noreply.github.com>
2023-01-20 09:48:35 -0500

    Fix python (#190)
    Fix notebook generation, and remove unnecessary pinned older versions of package.
    Testing is done in a container now.

--- test/dbinfo.py
+++ test/dbinfo.py
@@ -1,8 +1,8 @@
 from aperturedb import Connector
 # This file containts information on to access the server
 
-DB_HOST     = "localhost"
-DB_PORT     = 55557
+DB_HOST     = "aperturedb"
+DB_PORT     = 55555
 DB_USER     = "admin"
 DB_PASSWORD = "admin"
 
