ec7e93e9a52123b1315cd85ae5fa76b382dc6864
Gautam Saluja <52312085+gsaluja9@users.noreply.github.com>
2023-01-30 13:20:25 -0500

    Bigquery compatible version. (#198)

--- setup.py
+++ setup.py
@@ -1,18 +1,27 @@
+import os
 import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+OPENCV_VERSION = os.getenv("OPENCV_VERSION")
+
+install_requires = ['scikit-image', 'image', 'requests', 'boto3',
+                    'numpy', 'matplotlib', 'pandas', 'kaggle', 'google-cloud-storage',
+                    'dask[complete]==2023.1.0', 'ipywidgets',
+                    # Pinning this to be able to install tensorflow.
+                    'protobuf<3.20.0',
+                    # Pinning this to be able to install google-cloud-bigquery
+                    'grpcio-status==1.48.2'
+                    ]
+if OPENCV_VERSION is None:
+    install_requires.append('opencv-python')
+
 setuptools.setup(
     name="aperturedb",
     version="0.3.7",
     description="ApertureDB Client Module",
-    install_requires=['scikit-image', 'image', 'requests', 'boto3',
-                      'opencv-python', 'numpy', 'matplotlib', 'pandas', 'kaggle', 'google-cloud-storage',
-                      'dask[complete]', 'ipywidgets',
-                      # Pinning this to be able to install tensorflow.
-                      'protobuf<3.20.0'
-                      ],
+    install_requires=install_requires,
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/aperture-data/aperturedb-python",
