a057de0b98b2494ad2618d3256bed3fb67027893
Drew Ogle <drew@aperturedata.io>
2023-03-20 11:15:48 -0400

    add to stats

--- aperturedb/Stats.py
+++ aperturedb/Stats.py
@@ -2,10 +2,13 @@ class Stats:
     total_actions = 0
     times_arr = []
     total_actions_time = 0
-    error_counter  = 0
+    objects_existed  = 0
+    queries_suceeded  = 0
 
     def __init__(self):
         self.total_actions = 0
         self.times_arr = []
         self.total_actions_time = 0
         self.error_counter = 0
+        self.objects_existed = 0
+        self.queries_suceeded = 0
