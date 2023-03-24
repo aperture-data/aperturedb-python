4b5c39e7aa824a8e810d60d4d0356a51737de407
luisremis <luis@aperturedata.io>
2023-03-24 07:28:55 -0700

    Update Blobs.py

--- aperturedb/Blobs.py
+++ aperturedb/Blobs.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from aperturedb.Entities import Entities
 
+# This change will be reverted. It is just to test CI.
 
 class Blobs(Entities):
     db_object = "_Blob"
