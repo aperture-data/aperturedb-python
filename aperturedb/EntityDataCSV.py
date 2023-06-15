34cee11412ac9a3cfde702b9ba31b018cda11643
Drew Ogle <drew@aperturedata.io>
2023-06-15 12:16:12 -0400

    change back to not filtering for dask

--- aperturedb/EntityDataCSV.py
+++ aperturedb/EntityDataCSV.py
@@ -44,14 +44,13 @@ class EntityDataCSV(CSVParser.CSVParser):
 
     def __init__(self, filename, df=None, use_dask=False):
         super().__init__(filename, df=df, use_dask=use_dask)
-        if not use_dask:
-            self.props_keys       = [x for x in self.header[1:]
-                                     if not x.startswith(CSVParser.CONSTRAINTS_PREFIX)]
-            self.constraints_keys = [x for x in self.header[1:]
-                                     if x.startswith(CSVParser.CONSTRAINTS_PREFIX)]
-            self.command = "AddEntity"
-            self.search_keys = [x for x in self.header[1:]
-                                     if x.startswith(CSVParser.SEARCH_PREFIX)]
+        self.props_keys       = [x for x in self.header[1:]
+                                 if not x.startswith(CSVParser.CONSTRAINTS_PREFIX)]
+        self.constraints_keys = [x for x in self.header[1:]
+                                 if x.startswith(CSVParser.CONSTRAINTS_PREFIX)]
+        self.command = "AddEntity"
+        self.search_keys = [x for x in self.header[1:]
+                                 if x.startswith(CSVParser.SEARCH_PREFIX)]
 
     def get_indices(self):
         return {
