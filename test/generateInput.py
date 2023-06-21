d3009f86a28c53f01d9445e849096abfff0215b1
Drew Ogle <drew@aperturedata.io>
2023-06-20 20:48:48 -0400

    bigger input for testing

--- test/generateInput.py
+++ test/generateInput.py
@@ -466,7 +466,7 @@ def get_args():
     obj = argparse.ArgumentParser()
 
     # Run Config
-    obj.add_argument('-multiplier', type=int, default=2)
+    obj.add_argument('-multiplier', type=int, default=4)
 
     params = obj.parse_args()
 
