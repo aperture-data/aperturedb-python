9eaf7c8ceb25ea562823c08d2aec2936f60e02de
Gautam Saluja <gautam@aperturedata.io>
2023-07-06 12:44:20 -0400

    Compatibility with pydantic 2.0.2.

--- aperturedb/QueryTypes.py
+++ aperturedb/QueryTypes.py
@@ -1,39 +1,41 @@
 from __future__ import annotations
 from pydantic import BaseModel, Field
 from typing_extensions import Annotated
+from typing import ClassVar
 from uuid import uuid4
 from aperturedb.Query import ObjectType
 
 
 class IdentityModel(BaseModel):
     id: Annotated[str, Field(default_factory=lambda: uuid4().hex)]
-    type: Annotated[ObjectType, Field(
-        description="Object type")] = ObjectType.ENTITY
+    # Change as per the docs for the error
+    # https://docs.pydantic.dev/dev-v2/usage/errors/#model-field-overridden
+    type: ClassVar[ObjectType] = ObjectType.ENTITY
 
 
 class BlobModel(IdentityModel):
     file: Annotated[str, Field(title="Filepath", description="Path to file")]
-    type = ObjectType.BLOB
+    type = Field(ObjectType.BLOB)
 
 
 class ImageModel(BlobModel):
-    type = ObjectType.IMAGE
+    type = Field(ObjectType.IMAGE)
 
 
 class VideoModel(BlobModel):
-    type = ObjectType.VIDEO
+    type = Field(ObjectType.VIDEO)
 
 
 class DesctiptorModel(BlobModel):
-    type = ObjectType.DESCRIPTOR
+    type = Field(ObjectType.DESCRIPTOR)
 
 
 class PolygonModel(IdentityModel):
-    type = ObjectType.POLYGON
+    type = Field(ObjectType.POLYGON)
 
 
 class FrameModel(IdentityModel):
-    type = ObjectType.FRAME
+    type = Field(ObjectType.FRAME)
 
 
 class DescriptorSetModel(IdentityModel):
