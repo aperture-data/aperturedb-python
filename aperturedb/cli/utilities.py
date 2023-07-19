cb5a7551187c65824ad8cf5c752c787076e37a85
Gautam <gautam@aperturedata.io>
2023-07-18 22:04:30 -0400

    confirmation on remove_all

--- aperturedb/cli/utilities.py
+++ aperturedb/cli/utilities.py
@@ -1,6 +1,9 @@
-from aperturedb.Utils import create_connector, Utils
-import typer
 from enum import Enum
+
+import typer
+
+from aperturedb.Utils import Utils, create_connector
+from aperturedb.cli.console import console
 
 app = typer.Typer()
 
@@ -10,12 +13,22 @@ class CommandTypes(str, Enum):
     REMOVE_ALL = "remove_all"
 
 
+def confirm(command: CommandTypes):
+    console.print("Danger", style="bold red")
+    console.log(f"This will execute {command}.")
+    response = typer.prompt("Are you sure you want to continue? [y/N]")
+    if response.lower() != "y":
+        typer.echo("Aborting...")
+        raise typer.Abort()
+
+
 @app.command()
 def execute(command: CommandTypes):
     utils = Utils(create_connector())
     available_commands = {
         CommandTypes.SUMMARY: utils.summary,
-        CommandTypes.REMOVE_ALL: utils.remove_all_objects
+        CommandTypes.REMOVE_ALL: lambda: confirm(
+            CommandTypes.REMOVE_ALL) and utils.remove_all_objects
     }
 
     available_commands[command]()
