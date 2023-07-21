d5dfbb7c15224261f32b128807bc740079f5ba3e
Drew Ogle <104358188+drewaogle@users.noreply.github.com>
2023-07-21 13:13:07 -0400

    Dump server logs when tests fail (#260)
    * poke logs into test container to allow dump on failure

--- test/run_test.sh
+++ test/run_test.sh
@@ -19,6 +19,20 @@ echo "Running tests..."
 CREDENTIALS_FILE='/tmp/key.json'
 echo $GCP_SERVICE_ACCOUNT_KEY > $CREDENTIALS_FILE
 export GOOGLE_APPLICATION_CREDENTIALS=$CREDENTIALS_FILE
+# capture errors
+set +e
 PROJECT=aperturedata KAGGLE_username=ci KAGGLE_key=dummy coverage run -m pytest test_*.py -v
-echo "Generating coverage..."
-coverage html -i --directory=output
+RESULT=$?
+
+if [[ $RESULT != 0 ]]; then
+	echo "Test failed; outputting db log:"
+	if [[ "${APERTUREDB_LOG_PATH}" != "" ]]; then
+		cat -n "${APERTUREDB_LOG_PATH}"/aperturedb.INFO
+	else
+		echo "Unable to output log, APERTUREDB_LOG_PATH not set."
+	fi
+	exit 1
+else
+	echo "Generating coverage..."
+	coverage html -i --directory=output
+fi
