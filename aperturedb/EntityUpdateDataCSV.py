e662b0758c47ccbe5b76db0d19bcfc74b8995167
Drew Ogle <drew@aperturedata.io>
2023-07-28 23:15:20 -0400

    and here

--- aperturedb/EntityUpdateDataCSV.py
+++ aperturedb/EntityUpdateDataCSV.py
@@ -88,15 +88,14 @@ class SingleEntityUpdateDataCSV(CSVParser.CSVParser):
 
     def _setupkeys(self):
         if not self.keys_set:
-            if not self.use_dask:
-                self.keys_set = True
-                self.props_keys       = [x for x in self.header[1:]
-                                         if not (x.startswith(CSVParser.CONSTRAINTS_PREFIX)
-                                                 or x.startswith(SingleEntityUpdateDataCSV.UPDATE_CONSTRAINT_PREFIX))]
-                self.constraints_keys       = [x for x in self.header[1:]
-                                               if x.startswith(CSVParser.CONSTRAINTS_PREFIX)]
-                self.search_keys       = [x for x in self.header[1:]
-                                          if x.startswith(SingleEntityUpdateDataCSV.UPDATE_CONSTRAINT_PREFIX)]
+            self.keys_set = True
+            self.props_keys       = [x for x in self.header[1:]
+                                     if not (x.startswith(CSVParser.CONSTRAINTS_PREFIX)
+                                             or x.startswith(SingleEntityUpdateDataCSV.UPDATE_CONSTRAINT_PREFIX))]
+            self.constraints_keys       = [x for x in self.header[1:]
+                                           if x.startswith(CSVParser.CONSTRAINTS_PREFIX)]
+            self.search_keys       = [x for x in self.header[1:]
+                                      if x.startswith(SingleEntityUpdateDataCSV.UPDATE_CONSTRAINT_PREFIX)]
 
     def getitem(self, idx):
         idx = self.df.index.start + idx
