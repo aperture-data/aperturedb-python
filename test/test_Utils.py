5fc8a974663f68d3fada1c53b6be312e6525a9ae
Gautam Saluja <gautam@aperturedata.io>
2023-09-05 17:11:13 -0400

    Autopep changes.

--- test/test_Utils.py
+++ test/test_Utils.py
@@ -1,7 +1,7 @@
 class TestUtils():
 
     def test_remove_all_objects(self, utils):
-        assert utils.remove_all_objects() == True,\
+        assert utils.remove_all_objects() == True, \
             "Failed to remove all objects"
 
     def test_get_descriptorset_list(self, utils):
