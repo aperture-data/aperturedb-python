233f9397a64d651c6011a2df3043f2ef881b5db2
Ali Asadpoor <ali@aperturedata.io>
2023-09-29 11:58:40 -0400

    Use pre created load-balancer for route53 (#314)

--- deploy/modules/service/route53.tf
+++ deploy/modules/service/route53.tf
@@ -1,38 +1,11 @@
+data "aws_lb" "lb" {
+  name = "web-develop"
+}
 
 resource "aws_route53_record" "http-service" {
   zone_id = var.hosted-zone.zone_id
   name    = local.url
   type    = "CNAME"
   ttl     = 300
-  records = kubernetes_ingress_v1.server.status[0].load_balancer[0].ingress[*].hostname
-}
-
-resource "kubectl_manifest" "certificate-request" {
-  depends_on = [
-    aws_route53_record.http-service,
-  ]
-  yaml_body = yamlencode({
-    apiVersion = "cert-manager.io/v1"
-    kind       = "Certificate"
-    metadata = {
-      namespace = var.namespace
-      name      = var.service
-    }
-    spec = {
-      dnsNames = [local.url]
-      duration = "2160h0m0s"
-      issuerRef = {
-        kind = "ClusterIssuer"
-        name = local.cluster-issuer
-      }
-      renewBefore    = "360h0m0s"
-      secretName     = var.service
-      secretTemplate = {}
-      subject        = {}
-      usages = [
-        "server auth",
-        "client auth",
-      ]
-    }
-  })
+  records = [ data.aws_lb.lb.dns_name ]
 }
