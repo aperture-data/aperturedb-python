296953b16233989a070580ad43a56bb6832a6099
Ali Asadpoor <ali@aperturedata.io>
2023-10-26 19:34:20 -0400

    Build and push docker image with aperturedb package

--- publish.sh
+++ publish.sh
@@ -1,3 +1,11 @@
+echo "Building aperturedb"
 rm -rf build/ dist/ vdms.egg-info/
 python3 -m build
+
+echo "Uploading aperturedb"
 twine upload --skip-existing --verbose dist/*
+
+RELEASE_IMAGE="aperturedata/aperturedb-python:latest"
+echo "Building image ${RELEASE_IMAGE}"
+docker build --no-cache -t ${RELEASE_IMAGE} -f docker/release/Dockerfile .
+docker push ${RELEASE_IMAGE}
