d1056247f36f62e5d7950defe6ce8c22657bfe53
Gautam Saluja <52312085+gsaluja9@users.noreply.github.com>
2023-10-27 13:04:27 -0400

    Consistent naming of ApertureDB
    Co-authored-by: Vishakha Gupta-Cledat <24485122+vishakha041@users.noreply.github.com>

--- aperturedb/TensorFlowData.py
+++ aperturedb/TensorFlowData.py
@@ -31,7 +31,7 @@ class TensorFlowData(Subscriptable):
 
     def generate_query(self, idx: int) -> Tuple[List[dict], List[bytes]]:
         """
-        **Takes information from one atomic record from the Data and converts it to Query for apertureDB**
+        **Takes information from one atomic record from the Data and converts it to Query for ApertureDB**
 
         Args:
             idx (int): index of the record in collection.
