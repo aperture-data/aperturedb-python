0e97b693c6daeaf9ca39cff7e93cce308a13515f
Ali Asadpoor <ali@aperturedata.io>
2023-10-27 12:17:03 -0400

    Pass version to docker when publishing image

--- publish.sh
+++ publish.sh
@@ -6,6 +6,8 @@ echo "Uploading aperturedb"
 twine upload --skip-existing --verbose dist/*
 
 RELEASE_IMAGE="aperturedata/aperturedb-python:latest"
+source version.sh && read_version
 echo "Building image ${RELEASE_IMAGE}"
-docker build --no-cache -t ${RELEASE_IMAGE} -f docker/release/Dockerfile .
+docker build --no-cache -t ${RELEASE_IMAGE} \
+    --build-arg="VERSION=${BUILD_VERSION}" -f docker/release/Dockerfile .
 docker push ${RELEASE_IMAGE}
