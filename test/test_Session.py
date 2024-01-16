bbe3d59a869db5d623eed5d0477b79967c0b1005
Gautam Saluja <52312085+gsaluja9@users.noreply.github.com>
2024-01-16 08:14:10 -0800

    Feature/lazy connector (#365)
    and update the tests.

--- test/test_Session.py
+++ test/test_Session.py
@@ -20,35 +20,39 @@ class TestSession():
         """
         Verifies that Session renew works
         """
-        try:
-            # force session token expiry
-            db.shared_data.session.session_token_ttl = 1
-            logging.debug("Connected? {0}".format(
-                "yes" if db.connected else "no"))
 
-            logging.debug(
-                "Session valid? {0}".format(
-                    "yes" if db.shared_data.session.valid() else "no"))
-            logging.debug("Valid length: {0}".format(
-                db.shared_data.session.session_token_ttl))
-            time.sleep(2)
-            query = [{
-                "FindImage": {
-                    "results": {
-                        "limit": 5
-                    }
+        # force session token expiry
+        db.shared_data.session.session_token_ttl = 1
+        logger.debug("Connected? {0}".format(
+            "yes" if db.connected else "no"))
+        logger.debug(
+            "Session valid? {0}".format(
+                "yes" if db.shared_data.session.valid() else "no"))
+        logger.debug("Valid length: {0}".format(
+            db.shared_data.session.session_token_ttl))
+        time.sleep(2)
+        query = [{
+            "FindImage": {
+                "results": {
+                    "limit": 5
                 }
-            }]
-            responses, blobs = db.query(query)
-            logging.debug(responses)
-            assert db.shared_data.session.valid() == True
-        except Exception as e:
-            print(e)
-            print("Failed to renew Session")
-            assert False
+            }
+        }]
+        responses, blobs = db.query(query)
+        logger.debug(responses)
+        logger.debug("Valid : {0}".format(
+            db.shared_data.session.valid()))
+        assert db.shared_data.session.valid() == True
 
-    def test_SSL_error_on_query(self, db: Connector, monkeypatch):
-
+    def test_SSL_error_on_query(self, monkeypatch):
+        db = Connector(
+            dbinfo.DB_TCP_HOST,
+            dbinfo.DB_TCP_PORT,
+            dbinfo.DB_USER,
+            dbinfo.DB_PASSWORD,
+            retry_max_attempts=3,
+            retry_interval_seconds=0)
+        db.query([{"FindImage": {"results": {"limit": 5}}}])
         original_send_msg = db._send_msg
         count = 0
 
@@ -94,45 +98,48 @@ class TestSession():
                             lambda h, p: mock_connect(h, p))
 
         # Create new db connection.
+
+        new_db = Connector(
+            dbinfo.DB_TCP_HOST,
+            dbinfo.DB_TCP_PORT,
+            dbinfo.DB_USER,
+            dbinfo.DB_PASSWORD,
+            retry_max_attempts=3,
+            retry_interval_seconds=0)
         try:
-            new_db = Connector(
-                dbinfo.DB_TCP_HOST,
-                dbinfo.DB_TCP_PORT,
-                dbinfo.DB_USER,
-                dbinfo.DB_PASSWORD,
-                retry_connect_max_attempts=3,
-                retry_connect_interval_seconds=0)
+            new_db.query([{"FindImage": {"results": {"limit": 5}}}])
         except Exception as e:
             # Check the exception is not an obscure one.
-            assert str(e).startswith("Could not connect to apertureDB server:")
+            assert e.args[0] == "Authentication failed:"
 
         # Check that we tried to connect 3 times.
         assert connect_attempts == 3
 
     def test_socket_send_error_initial(self, monkeypatch):
-        connect_attempts = 0
+        send_attempts = 0
 
         def mock_send(x, buff):
-            nonlocal connect_attempts
-            connect_attempts += 1
+            nonlocal send_attempts
+            send_attempts += 1
             raise socket.error("Connection broke when send")
         monkeypatch.setattr(socket.socket, "send", mock_send)
 
         # Create new db connection.
+        new_db = Connector(
+            dbinfo.DB_TCP_HOST,
+            dbinfo.DB_TCP_PORT,
+            dbinfo.DB_USER,
+            dbinfo.DB_PASSWORD,
+            retry_max_attempts=3,
+            retry_interval_seconds=0)
         try:
-            new_db = Connector(
-                dbinfo.DB_TCP_HOST,
-                dbinfo.DB_TCP_PORT,
-                dbinfo.DB_USER,
-                dbinfo.DB_PASSWORD,
-                retry_connect_max_attempts=3,
-                retry_connect_interval_seconds=0)
+            new_db.query([{"FindImage": {"results": {"limit": 5}}}])
         except Exception as e:
             # Check the exception is not an obscure one.
-            assert str(e).startswith("Could not connect to apertureDB server:")
+            assert e.args[0] == "Authentication failed:"
 
-        # Check that we tried to connect 3 times.
-        assert connect_attempts == 3
+        # Check that we tried to send 5 (connect hello:2) + query:3) times.
+        assert send_attempts == 5
 
     def test_socket_recv_error_initial(self, monkeypatch):
         connect_attempts = 0
@@ -145,17 +152,19 @@ class TestSession():
         monkeypatch.setattr(socket.socket, "recv", mock_recv)
 
         # Create new db connection.
+
+        new_db = Connector(
+            dbinfo.DB_TCP_HOST,
+            dbinfo.DB_TCP_PORT,
+            dbinfo.DB_USER,
+            dbinfo.DB_PASSWORD,
+            retry_max_attempts=3,
+            retry_interval_seconds=0)
         try:
-            new_db = Connector(
-                dbinfo.DB_TCP_HOST,
-                dbinfo.DB_TCP_PORT,
-                dbinfo.DB_USER,
-                dbinfo.DB_PASSWORD,
-                retry_connect_max_attempts=3,
-                retry_connect_interval_seconds=0)
+            new_db.query([{"FindImage": {"results": {"limit": 5}}}])
         except Exception as e:
             # Check the exception is not an obscure one.
-            assert str(e).startswith("Could not connect to apertureDB server:")
+            assert e.args[0] == "Authentication failed:"
 
         # Check that we tried to connect 3 times.
         assert connect_attempts == 3
