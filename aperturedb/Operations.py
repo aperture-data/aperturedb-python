1145b4067d857751ef139063d2b6f93c64dc85f2
Gautam Saluja <52312085+gsaluja9@users.noreply.github.com>
2024-02-15 22:28:57 -0500

    ability to specify operations in python Query. (#383)

--- aperturedb/Operations.py
+++ aperturedb/Operations.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+
 class Operations(object):
     """
     **Operations that can be performed on the fly on any retrieved images**
@@ -12,7 +15,7 @@ class Operations(object):
     def get_operations_arr(self):
         return self.operations_arr
 
-    def resize(self, width, height):
+    def resize(self, width: int, height: int) -> Operations:
 
         op = {
             "type": "resize",
@@ -21,8 +24,9 @@ class Operations(object):
         }
 
         self.operations_arr.append(op)
+        return self
 
-    def rotate(self, angle, resize=False):
+    def rotate(self, angle: int, resize=False) -> Operations:
 
         op = {
             "type": "rotate",
@@ -31,3 +35,39 @@ class Operations(object):
         }
 
         self.operations_arr.append(op)
+        return self
+
+    def flip(self, code: str) -> Operations:
+
+        op = {
+            "type": "flip",
+            "code": code,
+        }
+
+        self.operations_arr.append(op)
+        return self
+
+    def crop(self, x: int, y: int, width: int, height: int) -> Operations:
+
+        op = {
+            "type": "crop",
+            "x": x,
+            "y": y,
+            "width": width,
+            "height": height,
+        }
+
+        self.operations_arr.append(op)
+        return self
+
+    def interval(self, start: int, stop: int, step: int) -> Operations:
+
+        op = {
+            "type": "interval",
+            "start": start,
+            "stop": stop,
+            "step": step
+        }
+
+        self.operations_arr.append(op)
+        return self
