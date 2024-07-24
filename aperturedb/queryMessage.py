86ff27ddd53cf766f1aad12cf736638d266827fe
Drew Ogle <drew@aperturedata.io>
2024-07-24 17:15:45 -0400

    formatting

--- aperturedb/queryMessage.py
+++ aperturedb/queryMessage.py
@@ -2,14 +2,17 @@
 import google.protobuf
 
 if google.protobuf.__version__.split(".")[0] == "3":
-    from  . import queryMessage3_pb2
+    from . import queryMessage3_pb2
     print("Selected v3")
+
     def queryMessage():
         return queryMessage3_pb2.queryMessage()
 elif google.protobuf.__version__.split(".")[0] == "4":
     from . import queryMessage4_pb2
     print("Selected v4")
+
     def queryMessage():
         return queryMessage4_pb2.queryMessage()
 else:
-    raise Exception(f"aperturedb not comaptible with {google.protobuf.__version__}")
+    raise Exception(
+        f"aperturedb not comaptible with {google.protobuf.__version__}")
