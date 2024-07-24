dde15080af55f214a5eec2b36f0a4765c07b73e3
Drew Ogle <drew@aperturedata.io>
2024-07-24 17:15:02 -0400

    add shim file

---
+++ aperturedb/queryMessage.py
@@ -0,0 +1,15 @@
+# queryMessage.py - wraps protobuf versions
+import google.protobuf
+
+if google.protobuf.__version__.split(".")[0] == "3":
+    from  . import queryMessage3_pb2
+    print("Selected v3")
+    def queryMessage():
+        return queryMessage3_pb2.queryMessage()
+elif google.protobuf.__version__.split(".")[0] == "4":
+    from . import queryMessage4_pb2
+    print("Selected v4")
+    def queryMessage():
+        return queryMessage4_pb2.queryMessage()
+else:
+    raise Exception(f"aperturedb not comaptible with {google.protobuf.__version__}")
