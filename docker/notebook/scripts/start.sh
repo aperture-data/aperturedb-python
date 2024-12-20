afba83a44271a1d5324f6ffbdb4c05f5abb0bea9
Ali Asadpoor <ali@aperturedata.io>
2024-12-20 11:32:55 -0500

    Allow overriding base url via envar (#543)

--- docker/notebook/scripts/start.sh
+++ docker/notebook/scripts/start.sh
@@ -4,8 +4,12 @@
 jupyter lab --generate-config
 
 PASS_HASH=$(python3 -c "from jupyter_server.auth import passwd; print(passwd('${PASSWORD:-test}'))")
-echo "c.NotebookApp.password='${PASS_HASH}' ">> /root/.jupyter/jupyter_lab_config.py
+echo "c.NotebookApp.password='${PASS_HASH}'">> /root/.jupyter/jupyter_lab_config.py
+if [ "${BASE_URL}" != "" ]
+then
+    echo "c.ServerApp.base_url='${BASE_URL}'">> /root/.jupyter/jupyter_lab_config.py
+fi
 
-mkdir /notebooks
+mkdir -p /notebooks
 cd /notebooks
 jupyter-lab --port=8888 --no-browser --allow-root --ip=0.0.0.0
