720623bf33bf861d3b0862eb43788d9fa9e5befb
Ali Asadpoor <ali@aperturedata.io>
2025-02-26 13:03:06 -0500

    Separate default values from commands

--- docker/notebook/scripts/start.sh
+++ docker/notebook/scripts/start.sh
@@ -3,12 +3,14 @@
 # Configure the Juypter Notebook password
 jupyter lab --generate-config
 
-echo "c.ServerApp.base_url='${BASE_URL:-/}'">> /root/.jupyter/jupyter_lab_config.py
-
-PASS_HASH=$(python3 -c "from jupyter_server.auth import passwd; print(passwd('${PASSWORD:-test}'))")
+PASSWORD=${PASSWORD:-test}
+PASS_HASH=$(python3 -c "from jupyter_server.auth import passwd; print(passwd('${PASSWORD}'))")
 echo "c.NotebookApp.password='${PASS_HASH}'">> /root/.jupyter/jupyter_lab_config.py
 
-NOTEBOOK_DIR=${notebook_dir:-/notebooks}
+BASE_URL=${BASE_URL:-/}
+echo "c.ServerApp.base_url='${BASE_URL}'">> /root/.jupyter/jupyter_lab_config.py
+
+NOTEBOOK_DIR=${NOTEBOOK_DIR:-/notebooks}
 mkdir -p ${NOTEBOOK_DIR}
 echo "c.NotebookApp.notebook_dir='${NOTEBOOK_DIR}'">> /root/.jupyter/jupyter_lab_config.py
 
