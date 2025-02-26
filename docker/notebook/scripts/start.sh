61b774735fd94c4d1f25493cda048fded47be86d
Ali Asadpoor <ali@aperturedata.io>
2025-02-26 12:53:10 -0500

    Allow notbook_dir to be configured

--- docker/notebook/scripts/start.sh
+++ docker/notebook/scripts/start.sh
@@ -3,13 +3,14 @@
 # Configure the Juypter Notebook password
 jupyter lab --generate-config
 
+echo "c.ServerApp.base_url='${BASE_URL:-/}'">> /root/.jupyter/jupyter_lab_config.py
+
 PASS_HASH=$(python3 -c "from jupyter_server.auth import passwd; print(passwd('${PASSWORD:-test}'))")
 echo "c.NotebookApp.password='${PASS_HASH}'">> /root/.jupyter/jupyter_lab_config.py
-if [ "${BASE_URL}" != "" ]
-then
-    echo "c.ServerApp.base_url='${BASE_URL}'">> /root/.jupyter/jupyter_lab_config.py
-fi
 
-mkdir -p /notebooks
-cd /notebooks
+NOTEBOOK_DIR=${notebook_dir:-/notebooks}
+mkdir -p ${NOTEBOOK_DIR}
+echo "c.NotebookApp.notebook_dir='${NOTEBOOK_DIR}'">> /root/.jupyter/jupyter_lab_config.py
+
+cd ${HOME}
 jupyter-lab --port=8888 --no-browser --allow-root --ip=0.0.0.0
