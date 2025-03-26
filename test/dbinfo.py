a634f4a5fb09009487d478c92cac64ee13641f07
Gautam Saluja <gautam@aperturedata.io>
2025-03-26 14:11:56 -0400

    Use localhost and the exposed ports for running tests.
    Improves local dev experience.

--- test/dbinfo.py
+++ test/dbinfo.py
@@ -1,8 +1,8 @@
 # This file containts information on to access the server
 
-DB_TCP_HOST  = "lenz"
-DB_REST_HOST = "nginx"
-DB_TCP_PORT  = 55551
-DB_REST_PORT = 80
+DB_TCP_HOST  = "localhost"
+DB_REST_HOST = "localhost"
+DB_TCP_PORT  = 55556
+DB_REST_PORT = 8087
 DB_USER      = "admin"
 DB_PASSWORD  = "admin"
