6dad3014dc2fadba4f9cf3c20fb502961d7bb695
Gautam <gautam@aperturedata.io>
2025-03-29 07:26:44 -0400

    Explicitly managing the the docker network.

--- test/run_test_container.sh
+++ test/run_test_container.sh
@@ -3,13 +3,20 @@
 set -u
 set -e
 
-docker compose -f docker-compose.ci.yml down --remove-orphans
+#Ensure clean environment (as much as possible)
+docker compose -f docker-compose.yml down --remove-orphans
+docker network rm ${RUNNER_NAME}_default || true
 # ensure latest db
 docker compose pull
-rm -rf aperturedb/db
+
 rm -rf output
 mkdir -m 777 output
-docker compose -f docker-compose.ci.yml up -d
+
+docker network create ${RUNNER_NAME}_default
+GATEWAY=$(docker network inspect ${RUNNER_NAME}_default | jq -r .[0].IPAM.Config[0].Gateway)
+echo "Gateway: $GATEWAY"
+export GATEWAY
+docker compose -f docker-compose.yml up -d
 
 LOG_PATH="$(pwd)/aperturedb/logs"
 TESTING_LOG_PATH="/aperturedb/test/server_logs"
@@ -30,6 +37,7 @@ docker run \
     -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
     -e GCP_SERVICE_ACCOUNT_KEY="$GCP_SERVICE_ACCOUNT_KEY" \
     -e APERTUREDB_LOG_PATH="${TESTING_LOG_PATH}" \
+    -e GATEWAY=${GATEWAY} \
     $REPOSITORY
 
 echo "Tests completed"
