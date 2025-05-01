9650d04658400bc07ba2b7b9f095b0ce8c91687d
Drew Ogle <104358188+drewaogle@users.noreply.github.com>
2025-05-01 17:16:37 -0400

    Command line APERTUREDB_KEY support (#586)
    * add token commands
    
    * parse env variables in cli
    
    * move 'tokens' to 'keys'
    
    * re-organize code
    
    * change storage format
    
    * formatting
    
    * fix support for environment variables
    
    * formatting
    
    * Backgrounded version check.
    
    * typo
    
    * Revert "typo"
    
    This reverts commit a4c8794762f29e4199ddb83adcce1a04391c63ee.
    
    * Revert "Backgrounded version check."
    
    This reverts commit 6deb50e618316d1d4644c491bc359d323beb105e.
    
    * Lax timing requirements.
    
    * move functions to Utils
    
    * formatting
    
    ---------
    
    Co-authored-by: Gautam Saluja <gautam@aperturedata.io>

--- aperturedb/Configuration.py
+++ aperturedb/Configuration.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 
 import json
+import re
 from base64 import b64encode, b64decode
 
 APERTURE_CLOUD = ".cloud.aperturedata.io"
@@ -24,10 +25,13 @@ class Configuration:
     # Max number of attempts to retry the initial connection (0 means infinite)
     # This is useful when the aperturedb server is not ready yet.
     retry_max_attempts: int = 3
+    token: str = None
+    user_keys: dict = None
 
     def __repr__(self) -> str:
         mode = "REST" if self.use_rest else "TCP"
-        return f"[{self.host}:{self.port} as {self.username} using {mode} with SSL={self.use_ssl}]"
+        auth_mode = "token" if self.token is not None else "password"
+        return f"[{self.host}:{self.port} as {self.username} using {mode} with SSL={self.use_ssl} auth={auth_mode}]"
 
     def deflate(self) -> list:
         deflate_version = 1
@@ -41,22 +45,96 @@ class Configuration:
         encoded_key = b64encode(simplified.encode('utf-8')).decode('utf-8')
         return encoded_key
 
+    def has_user_keys(self) -> bool:
+        return self.user_keys is not None
+
+    def add_user_key(self, user, key):
+        if self.user_keys is None:
+            self.user_keys = dict()
+        if not user in self.user_keys:
+            self.user_keys[user] = []
+        self.user_keys[user].insert(0, key)
+
+    def get_user_key(self, for_user: str) -> str:
+        if self.user_keys is None or not (for_user in self.user_keys) \
+                or len(self.user_keys[for_user]) == 0:
+            return None
+        return self.user_keys[for_user][0]
+
+    def set_user_keys(self, keys: dict) -> None:
+        self.user_keys = keys
+
     @classmethod
-    def reinflate(cls, encoded_key: list) -> object:
-        decoded_key = b64decode(encoded_key.encode('utf-8'))
-        as_list = json.loads(decoded_key.decode('utf-8'))
-        if as_list[0] != 1:
-            raise ValueError(f"version identifier of configuration was"
-                             "{as_list[0]}, which is not supported")
-        host, port, username, password, name, use_ssl, \
-            use_rest, use_keepalive, retry_interval_seconds, \
-            retry_max_attempts = as_list[1:]
-        if host.startswith(AD_CLOUD_SCHEME):
-            host = host[len(AD_CLOUD_SCHEME):] + APERTURE_CLOUD
-        use_ssl = bool(use_ssl)
-        use_rest = bool(use_rest)
-        use_keepaliave = bool(use_keepalive)
-        c = Configuration(
-            host, port, username, password, name, use_ssl, use_rest, use_keepalive,
-            retry_interval_seconds, retry_max_attempts)
+    def create_web_token(cls, host: str, port: int,  token_string: str) -> None:
+        if token_string.startswith("adbp_"):
+            token_string = token_string[5:]
+
+        if host.endswith(APERTURE_CLOUD):
+            host = host[:-1 * len(APERTURE_CLOUD)]
+            m = re.match("(.*)\.farm(\d+)$", host)
+            if m is not None:
+                host = "{}.{}".format(m.group(1), int(m.group(2)))
+
+            host = "a://{}".format(host)
+
+        if port == 55555:
+            web_token_json = [2, host, token_string]
+        else:
+            web_token_json = [2, host, port, token_string]
+        simplified = json.dumps(web_token_json)
+        encoded = b64encode(simplified.encode('utf-8')).decode('utf-8')
+        return encoded
+
+    @classmethod
+    def reinflate(cls, encoded_str: list) -> object:
+        try:
+            decoded = b64decode(encoded_str.encode('utf-8'))
+            as_list = json.loads(decoded.decode('utf-8'))
+        except:
+            raise Exception(
+                "Unable to make configuration from the provided string")
+        version = as_list[0]
+        if version not in (1, 2):
+            raise ValueError("version identifier of configuration was"
+                             f"g{as_list[0]}, which is not supported")
+        if version == 1:
+            host, port, username, password, name, use_ssl, \
+                use_rest, use_keepalive, retry_interval_seconds, \
+                retry_max_attempts = as_list[1:]
+            if host.startswith(AD_CLOUD_SCHEME):
+                host = host[len(AD_CLOUD_SCHEME):] + APERTURE_CLOUD
+            use_ssl = bool(use_ssl)
+            use_rest = bool(use_rest)
+            use_keepaliave = bool(use_keepalive)
+            c = Configuration(
+                host, port, username, password, name, use_ssl, use_rest, use_keepalive,
+                retry_interval_seconds, retry_max_attempts)
+        elif version == 2:
+            host = as_list[1]
+            if host.startswith("a://"):
+                m = re.match("a://([^.]*)\.(\d+)", host)
+                host = "{}.farm{:04d}.cloud.aperturedata.io".format(
+                    m.group(1), int(m.group(2)))
+
+            cur_arg = 2
+            # second arg is port
+            if isinstance(as_list[2], int):
+                cur_arg = 3
+            else:
+                port = 55555
+
+            name = "default"
+
+            username = None
+            password = None
+            token = None
+            # if 1 argument left, treat as token.
+            if len(as_list) - cur_arg == 1:
+                token = "adbp_" + as_list[cur_arg]
+            else:
+                username = as_list[cur_arg]
+                password = as_list[cur_arg + 1]
+
+            c = Configuration(host, port, username,
+                              password, name, token = token)
         return c
