e3804987f9c35332d89959b3c9c936f0e2c622f7
Drew Ogle <drew@aperturedata.io>
2025-05-01 15:49:47 -0400

    move functions to Utils

--- aperturedb/cli/keys.py
+++ aperturedb/cli/keys.py
@@ -4,36 +4,25 @@ from typing import Annotated
 import typer
 
 from aperturedb.cli.console import console
-import aperturedb.cli.tokens as tokens
 from aperturedb.Configuration import Configuration
 from aperturedb.Connector import Connector
 from aperturedb.CommonLibrary import create_connector
+from aperturedb.Utils import Utils
 
 app = typer.Typer()
-
-
-@app.command(help="List Keys")
-def list(user: Annotated[str, typer.Argument(help="The message to log")]):
-    conn = create_connector()
-    res, _ = conn.query([{"GetUserDetails": {"username": user}}])
-    utokens = res[0]['GetUserDetails']['tokens']
-    if len(utokens) == 0:
-        print(f"No Tokens for {user}")
-    else:
-        print(utokens)
-    pass
 
 
 @app.command(help="Create Key for a user")
 def generate(user: Annotated[str, typer.Argument(help="The user to generate a key for")]):
     conn = create_connector()
     key = generate_user_key(conn, user)
-    print(f"{key}")
+    console.log(f"Key for {user} is",key, highlight=False)
 
 
 def generate_user_key(conn: Connector, user: str):
-    token = tokens.generate_token(conn)
-    tokens.assign_token(conn, user, token)
+    u = Utils(conn)
+    token = u.generate_token()
+    u.assign_token( user, token)
     key = Configuration.create_web_token(
         conn.config.host, conn.config.port, token)
     return key
--- aperturedb/cli/tokens.py
+++ aperturedb/cli/tokens.py
@@ -4,71 +4,52 @@ from typing import Annotated
 import typer
 
 from aperturedb.cli.console import console
-from aperturedb.Configuration import Configuration
-from aperturedb.CommonLibrary import create_connector
-from aperturedb.Connector import Connector
+from aperturedb.CommonLibrary import create_connector,execute_query
+from aperturedb.Utils import Utils
 
 app = typer.Typer()
 
 
 @app.command(help="List User Authentication Tokens")
 def list(user: Annotated[str, typer.Argument(help="The user the display tokens for")]):
-    conn = create_connector()
-    res, _ = conn.query([{"GetUserDetails": {"username": user}}])
-    utokens = res[0]['GetUserDetails']['tokens']
+    token_list_query= [{"GetUserDetails": {"username": user}}]
+    client = create_connector()
+    result, response, blobs = execute_query(
+        client=client,
+        query=token_list_query,
+        blobs=[])
+    utokens = response[0]['GetUserDetails']['tokens']
     if len(utokens) == 0:
-        print(f"No Tokens for {user}")
+        console.log(f"No Tokens for {user}")
     else:
-        print(utokens)
-
+        console.log(utokens)
 
 @app.command(help="Generate an Authentication token for a user")
-def generate(silent: Annotated[bool, typer.Argument(help="Only return token, don't print it.")] = False):
+def generate():
     conn = create_connector()
-    token = generate_token(conn)
-    if not silent:
-        print(f"Created Access Token: {token}")
+    u = Utils(conn)
+    token = u.generate_token()
+    print(f"{token}")
     return token
-
-
-def generate_token(conn: Connector):
-    token_cmd = [{"GenerateToken": {}}]
-    r, _ = conn.query(token_cmd)
-    created = r[0]["GenerateToken"]
-    return created['token']
-
 
 @app.command(help="Assign an Authentication token to a user")
 def assign(user: Annotated[str, typer.Argument(help="user to assign the token to")],
            token: Annotated[str, typer.Argument(help="Token to be assigned")]):
     conn = create_connector()
+    u = Utils(conn)
     try:
-        assign_token(conn, user, token)
-        print(f"Assigned token to {user}")
+        u.assign_token( user, token)
+        console.log(f"Assigned token to {user}")
     except Exception as e:
-        print(f"Failed to assign token: {e}")
-
-
-def assign_token(conn: Connector, user: str, token: str):
-    token_cmd = [{"UpdateUser": {"username": user, "add_tokens": [token]}}]
-    r, _ = conn.query(token_cmd)
-    if isinstance(r, dict):
-        raise Exception(f"Error assigning token: {r}")
-    assigned = r[0]["UpdateUser"]
-    if assigned["status"] != 0:
-        raise Exception(
-            f"Error assigning token: Non-zero status: {assigned['status']}")
-
+        console.log(f"Failed to assign token: {e}", style="red")
 
 @app.command(help="Remove an Authentication token from a user")
 def remove(user: Annotated[str, typer.Argument(help="User to remove a token from")],
            token: Annotated[str, typer.Argument(help="Token to be removed")]):
-    from aperturedb.CommonLibrary import create_connector
-    token_cmd = [{"UpdateUser": {"username": user, "remove_tokens": [token]}}]
     conn = create_connector()
-    r, _ = conn.query(token_cmd)
-    removed = r[0]["UpdateUser"]
-    if removed["status"] == 0:
-        print("Action complete")
-    else:
-        print(f"Error removing token: removed['info']")
+    u = Utils(conn)
+    try:
+        u.remove_token(user,token)
+        console.log("Action complete")
+    except Exception as e:
+        console.log(f"Failed to remove token: {e}",style="red")
