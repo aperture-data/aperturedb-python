91680f8cfff610c2dc8784ec3521dd03107fcf0d
Gautam Saluja <gautam@aperturedata.io>
2025-05-15 22:16:49 -0400

    reordering imports to speed up adb config command times.

--- aperturedb/cli/keys.py
+++ aperturedb/cli/keys.py
@@ -1,4 +1,3 @@
-from enum import Enum
 from typing import Annotated
 
 import typer
@@ -6,20 +5,20 @@ import typer
 from aperturedb.cli.console import console
 from aperturedb.Configuration import Configuration
 from aperturedb.Connector import Connector
-from aperturedb.CommonLibrary import create_connector
-from aperturedb.Utils import Utils
 
 app = typer.Typer()
 
 
 @app.command(help="Create Key for a user")
 def generate(user: Annotated[str, typer.Argument(help="The user to generate a key for")]):
+    from aperturedb.CommonLibrary import create_connector
     conn = create_connector()
     key = generate_user_key(conn, user)
     console.log(f"Key for {user} is", key, highlight=False)
 
 
 def generate_user_key(conn: Connector, user: str):
+    from aperturedb.Utils import Utils
     u = Utils(conn)
     token = u.generate_token()
     u.assign_token(user, token)
