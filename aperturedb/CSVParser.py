671afed2dfcd5166fb45feeaffcb242ee64af92f
Drew Ogle <drew@aperturedata.io>
2025-07-10 20:13:26 -0400

    formatting

--- aperturedb/CSVParser.py
+++ aperturedb/CSVParser.py
@@ -79,8 +79,9 @@ class CSVParser(Subscriptable):
         # we expect the df index to have 'start', which means RangeIndex.
         # most users don't supply their own df, so this is mostly a sanity check
         # for when an advanced user has done filtering and have a IntervalIndex.
-        if not isinstance(self.df.index,pd.RangeIndex):
-            raise Exception(f"CSVParser requires a RangeIndex. the supplied DataFrame has a {type(self.df.index)} index.")
+        if not isinstance(self.df.index, pd.RangeIndex):
+            raise Exception(
+                f"CSVParser requires a RangeIndex. the supplied DataFrame has a {type(self.df.index)} index.")
         # len for dask dataframe needs a client.
         if not self.use_dask and len(self.df) == 0:
             logger.error("Dataframe empty. Is the CSV file ok?")
