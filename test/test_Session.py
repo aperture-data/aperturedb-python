f45ef6f30695c6c4ea1128fa53f7aa0ba160e9c1
ad-claw000 <claw000@aperturedata.io>
2026-04-21 00:26:29 +0000

    debug: add prints to test_sessionRenew

--- test/test_Session.py
+++ test/test_Session.py
@@ -39,6 +39,9 @@ class TestSession():
             }
         }]
         responses, blobs = db.query(query)
+        logger.debug(f"DEBUG responses: {responses}")
+        logger.debug(f"DEBUG connected: {db.connected}")
+        logger.debug(f"DEBUG session token: {db.shared_data.session.session_token}")
         logger.debug(responses)
         logger.debug("Valid : {0}".format(
             db.shared_data.session.valid()))
@@ -85,6 +88,9 @@ class TestSession():
             }
         }]
         responses, blobs = db.query(query)
+        logger.debug(f"DEBUG responses: {responses}")
+        logger.debug(f"DEBUG connected: {db.connected}")
+        logger.debug(f"DEBUG session token: {db.shared_data.session.session_token}")
         logging.debug(responses)
         assert db.shared_data.session.valid() == True
 
