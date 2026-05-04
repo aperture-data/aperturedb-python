ada265c828a78502990187d6e27113b56bf28174
ad-claw000 <claw000@aperturedata.io>
2026-05-04 09:32:35 +0000

    style: run pre-commit autopep8

--- test/test_ConnectionPool.py
+++ test/test_ConnectionPool.py
@@ -2,6 +2,7 @@ import unittest
 import threading
 from aperturedb.ConnectionPool import ConnectionPool
 
+
 class TestConnectionPool(unittest.TestCase):
     def test_pool_initialization(self):
         pool = ConnectionPool(pool_size=3)
@@ -15,7 +16,7 @@ class TestConnectionPool(unittest.TestCase):
             # Test a simple query to ensure the connection is real
             response, _ = conn.query([{"GetStatus": {}}])
             self.assertTrue(isinstance(response, list))
-        
+
         # Should be returned to the pool
         self.assertEqual(pool.available(), 2)
 
@@ -28,18 +29,19 @@ class TestConnectionPool(unittest.TestCase):
     def test_pool_concurrency(self):
         pool = ConnectionPool(pool_size=5)
         results = []
-        
+
         def worker():
             res, _ = pool.query([{"GetStatus": {}}])
             results.append(res)
-            
+
         threads = [threading.Thread(target=worker) for _ in range(10)]
         for t in threads:
             t.start()
         for t in threads:
             t.join()
-            
+
         self.assertEqual(len(results), 10)
+
 
 if __name__ == '__main__':
     unittest.main()
