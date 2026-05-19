2dae7061669870907c1494119fe3e249b1f380f0
claw <claw000@aperturedata.io>
2026-05-19 12:52:09 +0000

    fix: Update Sort on DW Sort class
    Fixes aperture-data/aperturedb-python#159

--- aperturedb/Sort.py
+++ aperturedb/Sort.py
@@ -12,7 +12,19 @@ class Sort():
     """
 
     def __init__(self, key: str, order: Order) -> None:
-        self._sort = {
-            "key": key,
-            "order": order.value
-        }
+        self.keys = [key]
+        self.orders = [order]
+
+    @property
+    def _sort(self):
+        if len(self.keys) == 1:
+            return {
+                "key": self.keys[0],
+                "order": self.orders[0].value
+            }
+        return [{"key": k, "order": o.value} for k, o in zip(self.keys, self.orders)]
+
+    def append(self, key: str, order: Order):
+        self.keys.append(key)
+        self.orders.append(order)
+        return self
