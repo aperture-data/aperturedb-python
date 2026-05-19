6e78ef98a306daaf57c73d29813b9e52a5de4140
ad-claw000 <claw000@aperturedata.io>
2026-05-19 09:24:06 +0000

    test: fix mock_query to return one response per command

--- test/test_ResponseHandler.py
+++ test/test_ResponseHandler.py
@@ -180,7 +180,7 @@ class QGSetPersonAndImages(Subscriptable):
 
 # Fake DB query.
 def mock_query(self, request, blobs):
-    self.response = [{k: {"status": 0} for c in request for k, v in c.items()}]
+    self.response = [{k: {"status": 0} for k, v in c.items()} for c in request]
     return self.response, []
 
 # Fake DB query. Depending in the transaction, return a response increasing number of images.
