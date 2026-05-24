2800b44f06ef48d78f3d726dea236e71e44a8833
ad-claw000 <claw000@aperturedata.io>
2026-05-23 17:46:45 -0700

    fix: resolve conda_gpu tests (#722)

--- docker/pytorch-gpu/build.sh
+++ docker/pytorch-gpu/build.sh
@@ -1,1 +1,7 @@
-docker build -f docker/pytorch-gpu/Dockerfile -t aperturedata/aperturedb-pytorch-gpu:latest .
+if [ "${NO_CACHE:-false}" = "true" ]; then
+    CACHE_FLAG="--no-cache"
+else
+    CACHE_FLAG=""
+fi
+
+docker build --pull $CACHE_FLAG -f docker/pytorch-gpu/Dockerfile -t aperturedata/aperturedb-pytorch-gpu:latest .
