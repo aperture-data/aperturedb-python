dfd52b6dca4e1690ce1fe54f966f84e1d49f668f
ad-claw000 <claw000@aperturedata.io>
2026-05-24 19:11:22 -0700

    Fix: Make ApertureDBDataset applicable to Videos as well (#726)

--- test/test_tf_connector.py
+++ test/test_tf_connector.py
@@ -15,9 +15,13 @@ class TestTfDatasets():
 
         count = 0
         # Iterate over dataset.
-        for img, label in dataset:
-            if tf.size(img).numpy() == 0:
-                logger.error("Empty image?")
+        for data, label in dataset:
+            if data.dtype == tf.string:
+                size = tf.strings.length(data).numpy()
+            else:
+                size = tf.size(data).numpy()
+            if size == 0:
+                logger.error("Empty data?")
                 assert False
             count += 1
         assert count == expected_length
@@ -85,8 +89,8 @@ class TestTfDatasets():
 
         start = time.time()
         count = 0
-        for imgs, labels in batched_dataset:
-            count += tf.shape(imgs)[0].numpy()
+        for data, labels in batched_dataset:
+            count += tf.shape(data)[0].numpy()
         assert count == len_limit
 
         time_taken = time.time() - start
@@ -137,3 +141,61 @@ class TestTfDatasets():
             ), [{"FindImage": {"results": {"list": ["prop"]}}}], label_prop="prop")
             dataset_f = dataset_wrapper_f.get_dataset()
             assert dataset_f.element_spec[1].dtype == tf.float32
+
+    def test_findBlob(self, db, utils, insert_data_from_csv):
+        blobs, _ = insert_data_from_csv("./input/blobs.adb.csv")
+        assert len(blobs) > 0
+        query = [{
+            "FindBlob": {
+                "results": {}
+            }
+        }]
+
+        dataset_wrapper = ApertureDBTensorFlowDataset(
+            db, query, label_prop="license")
+        dataset = dataset_wrapper.get_dataset()
+
+        count = 0
+        for blob, label in dataset:
+            assert isinstance(blob.numpy(), bytes)
+            assert label.dtype == tf.int32
+            count += 1
+
+        assert count == utils.count_entities("_Blob")
+
+    def test_findVideo_mocked(self):
+        from unittest.mock import patch
+        import tensorflow as tf
+
+        class DummyClient:
+            def clone(self):
+                return self
+
+            def get_last_response_str(self):
+                return ""
+
+        query = [{"FindVideo": {"results": {"list": ["prop"]}}}]
+
+        with patch('aperturedb.TensorFlowDataset.execute_query') as mock_exec:
+            def side_effect(*args, **kwargs):
+                batch_dict = {"total_elements": 1}
+                entities = [{"prop": 1}]
+                r = [{"FindVideo": {"batch": batch_dict, "entities": entities}}]
+                b = [b"mock_video_bytes"]
+                return None, r, b
+
+            mock_exec.side_effect = side_effect
+            dataset_wrapper = ApertureDBTensorFlowDataset(
+                DummyClient(), query, label_prop="prop")
+            dataset = dataset_wrapper.get_dataset()
+
+            assert dataset.element_spec[1].dtype == tf.int32
+            assert dataset.element_spec[0].dtype == tf.string
+
+            count = 0
+            for data, label in dataset:
+                assert isinstance(data.numpy(), bytes)
+                assert data.numpy() == b"mock_video_bytes"
+                assert label.numpy() == 1
+                count += 1
+            assert count == 1
