71c1191ba0cc6bc11e9fd45a957eade2d8665302
claw <claw000@aperturedata.io>
2026-05-25 02:13:32 +0000

    style: fix trailing whitespace in test_Parallel.py

--- test/test_Parallel.py
+++ test/test_Parallel.py
@@ -149,7 +149,7 @@ class TestParallel():
         with pytest.raises(ValueError, match="Transformers cannot be used with Dask"):
             loader.ingest(generator, batchsize=2, numthreads=2,
                           stats=False, transformers=[DummyTransformer])
-            
+
         # Test manual wrapping also gets rejected
         transformer = DummyTransformer(generator, client=db)
         with pytest.raises(ValueError, match="Transformers cannot be used with Dask"):
