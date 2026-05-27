85fb21343007c2674d0c19ca5c09374031f1235c
claw <claw000@aperturedata.io>
2026-05-27 05:07:27 +0000

    test: re-add external_network marker to SPARQL tests

--- test/test_SPARQL.py
+++ test/test_SPARQL.py
@@ -70,6 +70,7 @@ def sparql(db):
     return sparql
 
 
+@pytest.mark.external_network
 @pytest.mark.parametrize("description,query", [
     ('Find all images with chicken and butter as ingredients',
      """
